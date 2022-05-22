9d1ed8bb3cb2537e11b32e657bffb7f8bd7c0e54
Anastasia Les <anastasia@sonarmedia.com>
2022-05-22 16:37:43 +0300

    delete button fix

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,14 +1,13 @@
-import { ListElement, DeleteBtn } from './ContactList.styled';
-import { useDeleteContactMutation } from 'redux/contactsSlice';
+import { ContactItem } from './ContactItem';
 
 export const ContactList = ({ listOfContacts }) => {
-  const [deleteContact, { isLoading: isDeliting}] = useDeleteContactMutation()
   return (
     <div>
       <h2>Contacts</h2>
        <ul>
-          {listOfContacts.map(contact => (
-            <ListElement key={contact.id}>
+        {listOfContacts.map(contact => (<ContactItem key={contact.id} {...contact} />))}
+            
+        {/* <ListElement key={contact.id}>
               {contact.name}: {contact.number}
               <DeleteBtn
                 type="button"
@@ -16,8 +15,8 @@ export const ContactList = ({ listOfContacts }) => {
               > {isDeliting ? 'Deliting...' : 'Delete'}
                 
               </DeleteBtn>
-            </ListElement>
-          ))}
+            </ListElement> */}
+          
         </ul>
     </div>
   );
