e7b52f3e3b6e7c76dbdef3ef7f92adaab61c1691
Anastasia Les <anastasia@sonarmedia.com>
2022-05-22 18:18:50 +0300

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import {
-//   persistStore,
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from 'redux-persist'
-
-// import { persistedMyContactsReducer } from './contactsSlice';
 import { pokemonApi } from 'components/pokemon';
 import { myContactsApi } from './contactsSlice';
 
 
 export const store = configureStore({
   reducer: {
-    // myContacts: persistedMyContactsReducer,
-    [pokemonApi.reducerPath]: pokemonApi.reducer,
     [myContactsApi.reducerPath]: myContactsApi.reducer,
   },
   middleware: (getDefaultMiddleware) => [
@@ -25,13 +12,4 @@ export const store = configureStore({
     pokemonApi.middleware,
     myContactsApi.middleware,
    ]
-        // getDefaultMiddleware().concat(pokemonApi.middleware),
-  // middleware: (getDefaultMiddleware) =>
-  //     getDefaultMiddleware(getDefaultMiddleware().concat(pokemonApi.middleware),
-  //     {serializableCheck: {
-  //       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-  //     },
-  //   }),
 })
-
-// export const persistor = persistStore(store);
